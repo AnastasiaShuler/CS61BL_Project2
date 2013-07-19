615e608ff7c9d65b8c8c0b0ebbc93b557c22b373
kchao1994 <kchao1994@gmail.com>
2013-07-18 22:39:37 -0700

    Update Proof.java
    Last update for the night of 7/18/13

--- Proof.java
+++ Proof.java
@@ -12,6 +12,7 @@ public class Proof {
 	public void extendProof (String x) throws IllegalLineException, IllegalInferenceException {
 		String[] parts = x.split(" ");
 		Stack charStack = new Stack();
+		
 		for(int i=0; i < parts.length; i++) {
 			if(parts[i].charAt(0) == '(' || parts[i].charAt(0) == '~') { //new expression
 				Expression myExpression = new Expression(parts[i]);
@@ -26,29 +27,47 @@ public class Proof {
 			}
 			
 			//concerns: how to check if parens are in right place? probably through the tree
-			
+
 			if (charStack.empty() == false) {
 				//if string had same number of parens, stack should be empty at end
 				//(same number of parens pushed on and popped off)
 				throw new IllegalLineException("Your parentheses do not match up.");
 			}
-			//evaluating inferences
-			else if (parts[0].equals("mp")) {
-				// something about modus ponens
-				return do_mp();
-			}
-			else if (parts[0].equals("mt")) {
-				// something about modus tollens
-				return do_mt();
-			}
-			else if (parts[0].equals("ic")) {
-				// something about implied construction
-				return do_ic();
-			}
-			else if (parts[0].equals("co")) {
-				// something about contradiction
-				return do_co();
 			
+			//evaluating inferences
+			else if(parts[0].equals("mp")) {
+				// something about modus ponens
+				Inference.mp();
+			}
+			else if(parts[0].equals("mt")) {
+				// somethin about modus tollens
+				Inference.mt();
+			}
+			else if(parts[0].equals("ic")) {
+				// something about implied construction
+				Inference.ic();
+			}
+			else if(parts[0].equals("co")) {
+				// something about contradiction
+				Inference.co();
+			}
+			
+			//evaluating reasons
+			else if(parts[0].equals("assume")) {
+				//something about assume
+			}
+			
+			else if(parts[0].equals("show")) {
+				//something about show
+			}
+			
+			//misc stuff
+			else if(parts[0].equals("repeat")) {
+				//something about repeat
+			}
+			
+			else if(parts[0].equals("print")) {
+				
 			}
 		}
 	}
