402f27653c70650a840749e46417b65a38b00ba2
kchao1994 <kchao1994@gmail.com>
2013-07-18 21:31:38 -0700

    Update Proof.java

--- Proof.java
+++ Proof.java
@@ -11,8 +11,28 @@ public class Proof {
 
 	public void extendProof (String x) throws IllegalLineException, IllegalInferenceException {
 		String[] parts = x.split(" ");
-		if(parts.length < 2) {
-			throw new IllegalLineException();
+		for(int i=0; i < parts.length; i++) {
+			if(parts[i].charAt(0) == '(' || parts[i].charAt(0) == '~') { //new expression
+				Expression myExpression = new Expression(parts[i]);
+			}
+			
+			//evaluating inferences
+			else if(parts[0].equals("mp")) {
+				// something about modus ponens
+				return do_mp();
+			}
+			else if(parts[0].equals("mt")) {
+				// somethin about modus tollens
+				return do_mt();
+			}
+			else if(parts[0].equals("ic")) {
+				// something about implied construction
+				return do_ic();
+			}
+			else if(parts[0.equals("co")) {
+				// something about contradiction
+				return do_co();
+			}
 		}
 	}
 
