1e07525abe5e768cb6d780f3a91ffd1b16630ee1
kchao1994 <kchao1994@gmail.com>
2013-07-20 16:37:48 -0700

    Update LineNumber.java

--- LineNumber.java
+++ LineNumber.java
@@ -1,10 +1,35 @@
 import java.util.ArrayList;
 
 public class LineNumber {
-  private ArrayList<Integer> numbers;
+	private ArrayList<Integer> numbers;
+	private boolean beginProof;
+	private boolean finishProof;
 	
-	public LineNumber() {
+	public LineNumber(boolean beginProof, boolean finishProof) {
+		this.beginProof = beginProof;
+		this.finishProof = finishProof;
 		numbers = new ArrayList<Integer>();
+	}
+	
+	public void setbeginProof(boolean newBoolean) {
+		this.beginProof = newBoolean;
+	}
+	
+	public void setfinishProoof(boolean newBoolean) {
+		this.finishProof = newBoolean;
+	}
+	
+	public LineNumber next() {
+		if(beginProof) {
+			numbers.add(1);
+		}
+		else if(finishProof) {
+			numbers.remove(numbers.size()-1);
+		}
+		else {
+			Integer newNum = numbers.get(numbers.size()-1) + 1;
+			numbers.set(numbers.size()-1, newNum);
+		}
 	}
 	
 	public String toString() {
