ca844495807b1c956dade95019919863140b69e6
kchao1994 <kchao1994@gmail.com>
2013-07-20 16:19:49 -0700

    Update LineNumber.java

--- LineNumber.java
+++ LineNumber.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+
 public class LineNumber {
+  private ArrayList<Integer> numbers;
+	
+	public LineNumber() {
+		numbers = new ArrayList<Integer>();
+	}
+	
+	public String toString() {
+		String result = "";
+		for(Integer num : numbers) {
+			result = result + num + ".";
+		}
+		result = result.substring(0, result.length() - 1);
+		return result;
+	}
 }
