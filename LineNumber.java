f4438babb3fac74b4ef9f69460175f3e1ee1d364
kchao1994 <kchao1994@gmail.com>
2013-07-22 16:04:31 -0700

    Update LineNumber.java

--- LineNumber.java
+++ LineNumber.java
@@ -9,6 +9,7 @@ public class LineNumber {
 		this.beginProof = beginProof;
 		this.finishProof = finishProof;
 		numbers = new ArrayList<Integer>();
+		numbers.add(1);
 	}
 	
 	public void setbeginProof(boolean newBoolean) {
@@ -20,15 +21,20 @@ public class LineNumber {
 	}
 	
 	public LineNumber next() {
-		if(beginProof) {
-			numbers.add(1);
+		if(numbers.size() <= 1) { //the very beginning "show" exception
+			numbers.set(0, numbers.get(0) + 1);
 		}
-		else if(finishProof) {
-			numbers.remove(numbers.size()-1);
-		}
-		else {
-			Integer newNum = numbers.get(numbers.size()-1) + 1;
-			numbers.set(numbers.size()-1, newNum);
+		else { //everything that happens after that
+			if(beginProof) {
+				numbers.add(1);
+			}
+			else if(finishProof) {
+				numbers.remove(numbers.size()-1);
+			}
+			else {
+				Integer newNum = numbers.get(numbers.size()-1) + 1;
+				numbers.set(numbers.size()-1, newNum);
+			}
 		}
 		return this;
 	}
