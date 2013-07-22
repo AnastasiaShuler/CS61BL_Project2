edd4c09aa5b5db9478bec1265bb916ec56f19058
jihern <jgbaek@berkeley.edu>
2013-07-22 16:42:26 -0700

    Update Expression.java

--- Expression.java
+++ Expression.java
@@ -1,6 +1,34 @@
+import java.util.ArrayList;
+
 public class Expression {
+	ArrayList <String> parsed = new ArrayList <String> ();
 	
+	//sends to our binary tree?
 	public Expression(String x) {
+		int length = x.length();
+		for (int i = 0; i < length; i++){
+			if (x.charAt(i) == '('){
+				parsed.add("(");
+			} else if (x.charAt(i) == '~'){
+				parsed.add("~");
+			} else if ((length - (i+2)) >= 0){
+			//catches index out of bounds scenario
+				if (x.substring(i, i+2).equals("=>")){
+					parsed.add("=>");
+					i++;
+				}
+			} else if(x.charAt(i) == '|') {
+	        	parsed.add("|");
+	        } else if(x.charAt(i) == '&') {
+	        	parsed.add("&");
+	        } else if (x.charAt(i) == ')'){
+	        	parsed.add(")");
+	        }
+	    }
+	}
+}
+	
+	/*public Expression(String x) {
 		 for (int i = x.length() - 1; i >= 0; --i) {
 	         if (x.substring(i, i+2) == "=>") {
 	             //detects implies =>
@@ -23,4 +51,4 @@ public class Expression {
 	         }
 	    }
 	}
-}
+}*/
