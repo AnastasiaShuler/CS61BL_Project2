99a90d7d5d4d08f9db64ed1a661ff54cda9d11e0
AnastasiaShuler <ashuler@berkeley.edu>
2013-07-22 15:44:19 -0700

    Adding the ProofSoFar class.

---
+++ ProofSoFar.java
@@ -0,0 +1,69 @@
+
+
+public class ProofSoFar{
+	public ProofNode myRoot;
+	
+	public ProofSoFar(){
+		myRoot = null;
+	}
+	
+	public void addNewLine(int lineNum){
+		ProofNode curr = myRoot;
+		while(lineNum > 1){
+			curr = curr.myNextLine;
+			lineNum --;
+		}
+		curr.myNextLine = new ProofNode();
+	}
+	
+	public void addNewSubline(String lineNum){
+		int count = 0;
+		ProofNode curr = myRoot;
+		while(2*count <= lineNum.length() - 2){
+			String num = lineNum.substring(2*count, 2*count + 1);
+			Integer line = Integer.parseInt(num);
+			while(line > 1){
+				curr = curr.myNextLine;
+				line --;
+			}
+			curr = curr.mySublines;
+		}
+		if(lineNum.substring(2*count).equals("1")){
+			curr.mySublines = new ProofNode();
+		} else{
+			Integer line = Integer.parseInt(lineNum.substring(2*count));
+			while(line > 1){
+				curr = curr.myNextLine;
+			}
+			curr.myNextLine = new ProofNode();
+		}
+		
+	}
+	
+	public void add(String s){
+		
+	}
+		
+	
+	private static class ProofNode{
+		protected ProofNode myNextLine;
+		protected ProofNode mySublines;
+		public String myString;
+		public ProofTree myProofTree;
+		
+		public ProofNode(){
+			myNextLine = null;
+			mySublines = null;
+			myString = null;
+			myProofTree = null;
+		}
+		
+		public ProofNode(String expr){
+			myNextLine = null;
+			mySublines = null;
+			myString = expr;
+			myProofTree = ProofTree.createATree(expr);
+		}
+		
+	}
+}
