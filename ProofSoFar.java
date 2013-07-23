426b1ee43647b76de7f4d53401158008c9cd612e
AnastasiaShuler <ashuler@berkeley.edu>
2013-07-22 17:32:02 -0700

    Fixed add() and find(). Still in Beta.

--- ProofSoFar.java
+++ ProofSoFar.java
@@ -10,19 +10,21 @@ public class ProofSoFar{
 	}
 	
 	public void add(String lineNum, String s){
+		System.out.println(lineNum);
 		if(lineNum.length() == 1){
 			int line = Integer.parseInt(lineNum);
 			mainNums.add(new ProofLineList(lineNum, s));
 		} else{
 			int line = Integer.parseInt(lineNum.substring(0,1));
-			mainNums.get(line).add(lineNum, s);
+			System.out.println(line);
+			mainNums.get(line-1).add(lineNum, s);
 		}
 		
 	}
 	
 	public String find(String lineNum){
 		int mainLine = Integer.parseInt(lineNum.substring(0,1));
-		return mainNums.get(mainLine).find(lineNum);
+		return mainNums.get(mainLine-1).find(lineNum);
 	}
 	
 	private static class ProofLineList{
@@ -39,8 +41,13 @@ public class ProofSoFar{
 		}
 		
 		public void add(String lineNum, String s){
-			myTail.next = new ProofNode(lineNum, s);
-			myTail = myTail.next;
+			if(myTail != null){
+				myTail.next = new ProofNode(lineNum, s);
+				myTail = myTail.next;
+			} else{
+				myHead = new ProofNode(lineNum, s);
+				myTail = myHead;
+			}
 			
 		}
 		
