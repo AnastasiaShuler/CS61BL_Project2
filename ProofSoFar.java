8f96361d0b595a481d3107672da976dd19ded5db
AnastasiaShuler <ashuler@berkeley.edu>
2013-07-22 17:18:32 -0700

    Added find() and add() methods.

--- ProofSoFar.java
+++ ProofSoFar.java
@@ -3,21 +3,56 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class ProofSoFar{
-	ArrayList mainNums;
+	ArrayList<ProofLineList> mainNums;
 	
 	public ProofSoFar(){
-		mainNums = new ArrayList();
+		mainNums = new ArrayList<ProofLineList>();
+	}
+	
+	public void add(String lineNum, String s){
+		if(lineNum.length() == 1){
+			int line = Integer.parseInt(lineNum);
+			mainNums.add(new ProofLineList(lineNum, s));
+		} else{
+			int line = Integer.parseInt(lineNum.substring(0,1));
+			mainNums.get(line).add(lineNum, s);
+		}
+		
+	}
+	
+	public String find(String lineNum){
+		int mainLine = Integer.parseInt(lineNum.substring(0,1));
+		return mainNums.get(mainLine).find(lineNum);
 	}
 	
 	private static class ProofLineList{
 		private ProofNode myHead;
+		private ProofNode myTail;
 		
 		public ProofLineList(){
 			myHead = null;
+			myTail = null;
 		}
 		
 		public ProofLineList(String lineNum, String s){
 			myHead = new ProofNode(lineNum, s);
+		}
+		
+		public void add(String lineNum, String s){
+			myTail.next = new ProofNode(lineNum, s);
+			myTail = myTail.next;
+			
+		}
+		
+		public String find(String lineNum){
+			ProofNode curr = myHead;
+			while(curr != null){
+				if(curr.myLineNumer.equals(lineNum)){
+					return curr.myString;
+				} else
+					curr=curr.next;
+			}
+			return null;
 		}
 		
 	}
@@ -47,122 +82,3 @@ public class ProofSoFar{
 	}
 	
 }
-
-/*
-public class ProofSoFar{
-	public ProofNode myRoot;
-	
-	public ProofSoFar(){
-		myRoot = null;
-	}
-	
-	public void addNewLine(int lineNum){
-		ProofNode curr = myRoot;
-		while(lineNum > 1){
-			curr = curr.myNextLine;
-			lineNum --;
-		}
-		curr.myNextLine = new ProofNode();
-	}
-	
-	public void addNewSubline(String lineNum){
-		int count = 0;
-		ProofNode curr = myRoot;
-		while(2*count <= lineNum.length() - 2){
-			String num = lineNum.substring(2*count, 2*count + 1);
-			Integer line = Integer.parseInt(num);
-			while(line > 1){
-				curr = curr.myNextLine;
-				line --;
-			}
-			curr = curr.mySublines;
-		}
-		if(lineNum.substring(2*count).equals("1")){
-			curr.mySublines = new ProofNode();
-		} else{
-			Integer line = Integer.parseInt(lineNum.substring(2*count));
-			while(line > 1){
-				curr = curr.myNextLine;
-			}
-			curr.myNextLine = new ProofNode();
-		}
-		
-	}
-	
-	public void add(String s, String lineNum){
-		int count = 0;
-		ProofNode curr = myRoot;
-		if(lineNum.equals("1")){
-			myRoot = new ProofNode(s);
-			return;
-		}
-		while(2*count <= lineNum.length() - 2){
-			String num = lineNum.substring(2*count, 2*count + 1);
-			Integer line = Integer.parseInt(num);
-			System.out.println(line);
-			while(line > 1){
-				curr = curr.myNextLine;
-				line --;
-			}
-			count ++;
-		}
-		int last = Integer.parseInt(lineNum.substring(lineNum.length() -1));
-		if(last == 1){
-			curr.mySublines = new ProofNode(s);
-		} else{
-			curr = curr.mySublines;
-			while(last > 2){
-				curr = curr.mySublines;
-				last --;
-			}
-			ProofNode toAdd = new ProofNode(s);
-			curr.mySublines = toAdd;
-		}
-	}
-	
-	public String get(String lineNum){
-		int count = 0;
-		ProofNode curr = myRoot;
-		while(2*count <= lineNum.length() - 2){
-			String num = lineNum.substring(2*count, 2*count + 1);
-			Integer line = Integer.parseInt(num);
-			System.out.println(line);
-			while(line > 1){
-				curr = curr.myNextLine;
-				line --;
-			}
-			count ++;
-		}
-		int last = Integer.parseInt(lineNum.substring(lineNum.length() -1));
-		curr = curr.mySublines;
-		while(last > 2){
-			curr = curr.mySublines;
-			last --;
-		}
-		return curr.myString;
-	}
-		
-	
-	private static class ProofNode{
-		protected ProofNode myNextLine;
-		protected ProofNode mySublines;
-		public String myString;
-		public ProofTree myProofTree;
-		
-		public ProofNode(){
-			myNextLine = null;
-			mySublines = null;
-			myString = null;
-			myProofTree = null;
-		}
-		
-		public ProofNode(String expr){
-			myNextLine = null;
-			mySublines = null;
-			myString = expr;
-			myProofTree = ProofTree.createATree(expr);
-		}
-		
-	}
-}
-*/
