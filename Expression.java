8c9c09d6e62ab88e40cce9bb12f831905f35bf1f
jihern <jgbaek@berkeley.edu>
2013-07-22 22:04:35 -0700

    Update Expression.java

--- Expression.java
+++ Expression.java
@@ -1,7 +1,16 @@
+package project2;
+
+import java.util.*;
+
+
 public class Expression {
 	//check if theorem name, or if a reason, etc. create an arraylist, if it's any of those words like "show" etc. return true;
 	boolean valid; 	
-	
+	Stack <Object >charStack = new Stack <Object> ();
+	//String [] keyWords = {"mp", "mt", "ic", "ct", "show", "assume"};
+	//better to use arraylist? how do we access this and add theorem names later?
+	//will have to do arraylist
+	ArrayList <String> keyWords = new ArrayList <String> ();
 	
 	public Expression (String x){
 		if (checkValidity(x)){
@@ -9,6 +18,12 @@ public class Expression {
 		} else {
 			valid = false;
 		}
+		keyWords.add("mp");
+		keyWords.add("mt");
+		keyWords.add("ic");
+		keyWords.add("ct");
+		keyWords.add("show");
+		keyWords.add("assume");
 	}
 	
 	public Boolean checkValidity(String expression) {
@@ -28,8 +43,11 @@ public class Expression {
 				return false;
 			}
 			
+		} else if(keyWords.contains(expression)){ 
+			return true;
+			
 		} else {
-
+			//checks if opening and closing parentheses are right next to each.
 			for (int k = 0; k < expression.length(); k++){
 				if (k <= expression.length()-1 && k+1 <= expression.length()-1){
 					if (expression.charAt(k) == '(' && expression.charAt(k+1) == ')'){
@@ -38,47 +56,28 @@ public class Expression {
 				}
 			}
 			
+			//checks parentheses are all matched (confirm with group if we want this check here or in proof)
+			for (int l = 0; l < expression.length(); l++){
+				if (expression.charAt(l) == '('){
+					charStack.push(expression.charAt(l));
+				} else if (expression.charAt(l) == ')'){
+					charStack.pop();
+				}	
+			}
 			
-		
-		//check that opening paranthesis is not right next to closing parenthesis
+			if (!charStack.isEmpty()){
+				return false;
+			}
 			
+			//check the implies
+			for (int m = 0; m < expression.length(); m++){
+				if (expression.charAt(m) == '='){
+					if ((m+1 <= expression.length()-1) && expression.charAt(m+1) != '>'){
+						return false;
+					}
+				}
+			}
 		}
+		return true;
 	}
 }
-		/*
-
-	            if(expr.length() == 3){
-	                TreeNode left = new TreeNode (expr.charAt(0));
-	                TreeNode operation = new TreeNode (expr.charAt(1));
-	                TreeNode right = new TreeNode (expr.charAt(2));
-	                return new TreeNode(operation, left, right);
-	            }
-	            //return new TreeNode(exprTreeHelper(expr), new TreeNode(expr.charAt(0)), new TreeNode(expr.charAt(expr.length())));
-	        } else {
-	            // expr is a parenthesized expression.
-	            // Strip off the beginning and ending parentheses,
-	            // find the main operator (an occurrence of + or * not nested
-	            // in parentheses, and construct the two subtrees.
-	            int nesting = 0;
-	            int opPos = 0;
-	            for (int k=1; k<expr.length()-1; k++) {
-	                if (expr.charAt(k) == '(') {
-	                    nesting++;
-	                } if (expr.charAt(k) == ')') {
-	                    nesting--;
-	                } if (nesting == 0 && (expr.charAt(k) == '+' || expr.charAt(k) == '*')) {
-	                    opPos = k;
-	                }  
-	            }
-	            String opnd1 = expr.substring (1, opPos);
-	            String opnd2 = expr.substring (opPos+1, expr.length()-1);
-	            String op = expr.substring (opPos, opPos+1);
-	            System.out.println ("expression = " + expr);
-	            System.out.println ("operand 1  = " + opnd1);
-	            System.out.println ("operator   = " + op);
-	            System.out.println ("operand 2  = " + opnd2);
-	            System.out.println ( );
-	            return new TreeNode(op, exprTreeHelper(opnd1), exprTreeHelper(opnd2));
-	        	}
-	        return null;
-	    }  */
