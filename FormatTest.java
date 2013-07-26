a5ce51d009cfbf6e2a05995674770adbeb5643ba
ijang <irisjang@berkeley.edu>
2013-07-25 20:30:43 -0700

    Update FormatTest.java

--- FormatTest.java
+++ FormatTest.java
@@ -1,53 +1,53 @@
 import junit.framework.TestCase;
 
 public class FormatTest extends TestCase {
-	
+
 	public void testSingleExprs() {
 		Format a = new Format();
-		String [] ex1 = new String[] {"(p=>q)"};
+		String ex1 = "(p=>q)";
 		assertTrue(a.checkFormat(ex1));
 	}
 
 	public void testKeywords () {
 		Format a = new Format();
-		String [] ex1 = new String [] {"mp", "2.1.3", "2.3.3", "(p=>q)"};
+		String ex1 = "mp 2.1.3 2.3.3 (p=>q)";
 		assertTrue(a.checkFormat(ex1));
-		String [] ex2 = new String [] {"co", "1", "5.1", "~p"};
+		String ex2 = "co 1 5.1 ~p";
 		assertTrue(a.checkFormat(ex2));
-		String [] ex3 = new String [] {"ic", "3.4", "7.9", "(a=>(a=>b))"};
+		String ex3 = "ic 3.4 7.9 (a=>(a=>b))";
 		assertFalse(a.checkFormat(ex3));
-		String [] ex4 = new String [] {"ic3.4", "7.9", "(a=>(a=>b))"};
+		String ex4 = "ic3.4 7.9 (a=>(a=>b))";
 		assertFalse(a.checkFormat(ex4));
-		String [] ex5 = new String [] {"antsy", "3.4", "7.9", "(a=>(a=>b))"};
+		String ex5 = "antsy 3.4 7.9 (a=>(a=>b))";
 		assertFalse(a.checkFormat(ex5));
 	}
 
 	public void testShowAssume() {
 		Format b = new Format();
-		String [] ex1 = new String [] {"show", "~q"};
+		String ex1 = "show ~q" ;
 		assertTrue(b.checkFormat(ex1));
-		String [] ex2 = new String [] {"assume", "(p=>q)"};
+		String ex2 = "assume (p=>q)";
 		assertTrue(b.checkFormat(ex2));
-		String [] ex3 = new String [] {"Beavis", "(p=>q)"};
-		assertTrue(b.checkFormat(ex3));
-		String [] ex4 = new String [] {"Beavis(p=>q)"};
-		//technically not true, but since only one part, this case is checked by Expression.
-		assertTrue(b.checkFormat(ex4));
 	}
 
 	public void testTheoremName() {
 		Format c = new Format();
-		String [] ex1 = new String [] {"patsy", "(~p=>q)"};
+		String ex1 = "patsy (~p=>q)";
 		assertTrue(c.checkFormat(ex1));
+		String ex2 = "Beavis (p=>q)";
+		assertTrue(c.checkFormat(ex2));
+		String ex3 = "Beavis(p=>q)";
+		//technically not true, but since only one part, this case is checked by Expression.
+		assertTrue(c.checkFormat(ex3));
 	}
-	
+
 	public void testCheckExpressions(){
 		Format check = new Format();
-		
+
 		//checks null input
 		//Expression a = new Expression(null);
 		//assertFalse(check.expressionValidity(a));
-		
+
 		//checks expression with a length of one
 		Expression b = new Expression("p");
 		assertTrue(check.expressionValidity(b));
@@ -77,7 +77,13 @@ public class FormatTest extends TestCase {
 		//We match up parens, but never check that there IS a closing paren
 		//in the right spot
 		//Only things that may follow a paren: => or )
-		assertTrue(check.expressionValidity(k));
+		assertFalse(check.expressionValidity(l));
+		Expression m = new Expression("(p=>(q))");
+		assertFalse(check.expressionValidity(m));
+		Expression n = new Expression("(p(=>q))");
+		assertFalse(check.expressionValidity(n));
+		Expression o = new Expression("(p= >q)");
+		assertFalse(check.expressionValidity(o));
 	}
-	
+
 }
