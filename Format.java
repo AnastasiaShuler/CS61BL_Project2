2c40f3f515024e3400196871631fb0d7d6906a13
ijang <irisjang@berkeley.edu>
2013-07-25 21:20:10 -0700

    Update Format.java

--- Format.java
+++ Format.java
@@ -1,236 +1,181 @@
-import java.util.Stack;
+import junit.framework.TestCase;
 
-public class Format {
+public class FormatTest extends TestCase {
 
-	Stack charStack = new Stack ();
-
-	/*
-	 * checkFormat checks user input for overall validity. 
-	 * For example, input beginning with "mp" must have
-	 * four sections (mp, line number, line number, expression).
-	 */
-
-	public boolean checkFormat (String wholeInput) {
-		/* This method implements a preliminary "sanity check" on the overall 
-		 * format of user input. If checkFormat(string) returns true, then call
-		 * expressionValidity(expressionString) to check the validity of 
-		 * solely the expression part of user input.
-		 */
-		String [ ] parts = wholeInput.split(" ");
-
-		if (parts.length == 1) {
-			/*
-			 * if only 1 "part," then let it be checked by Expression.
-			 */
-			return true;
-		}
-
-		if (parts.length > 1) {
-			/*
-			 * if there are more than one parts in an expression, 
-			 * the first part must be a keyword.
-			 */
-
-			if (parts[0].equals("mp") || parts[0].equals("co")) {
-				/*
-				 * These two keywords have the same format of
-				 * "mp/co" [line number] [line number] [expression],
-				 * so we can compound these into one check
-				 */
-				if (parts.length == 4) {
-					return true;
-				}
-			}
-
-			if (parts[0].equals("ic")) {
-				if (parts.length == 3) {
-					return true;
-				}
-			}
-
-			if (parts[0].equals("show") 
-			 || parts[0].equals("assume")
-			 || parts[0] instanceof String) {
-				if (parts.length == 2) {
-					return true;
-				}
-			}
-		}
-		return false;
+	public void testSingleExprs() {
+		Format a = new Format();
+		String ex1 = "(p=>q)";
+		assertTrue(a.checkFormat(ex1));
 	}
 
-	public boolean expressionValidity (String expr) throws IllegalLineException {
+	public void testKeywords () {
+		Format a = new Format();
+		String ex1 = "mp 2.1.3 2.3.3 (p=>q)";
+		assertTrue(a.checkFormat(ex1));
+		String ex2 = "co 1 5.1 ~p";
+		assertTrue(a.checkFormat(ex2));
+		String ex3 = "ic 3.4 7.9 (a=>(a=>b))";
+		assertFalse(a.checkFormat(ex3));
+		String ex4 = "ic3.4 7.9 (a=>(a=>b))";
+		assertFalse(a.checkFormat(ex4));
+		String ex5 = "antsy 3.4 7.9 (a=>(a=>b))";
+		assertFalse(a.checkFormat(ex5));
+	}
+
+	public void testShowAssume() {
+		Format b = new Format();
+		String ex1 = "show ~q" ;
+		assertTrue(b.checkFormat(ex1));
+		String ex2 = "assume (p=>q)";
+		assertTrue(b.checkFormat(ex2));
+	}
+
+	public void testTheoremName() {
+		Format c = new Format();
+		String ex1 = "patsy (~p=>q)";
+		assertTrue(c.checkFormat(ex1));
+		String ex2 = "Beavis (p=>q)";
+		assertTrue(c.checkFormat(ex2));
+		String ex3 = "Beavis(p=>q)";
+		//technically not true, but since only one part, this case is checked by Expression.
+		assertTrue(c.checkFormat(ex3));
+	}
+
+	public void testCheckExpressions () {
+		Format check = new Format();
+
+		//checks null input
+		//Expression a = new Expression(null);
+		//assertFalse(check.expressionValidity(a));
+
+		//checks expression with a length of one
+		String b = new String("p");
+		try {
+			check.expressionValidity(b);
+		}
+		catch (IllegalLineException e) {
+			assert true;
+		}
 		
-		/* expressionValidity checks the expression section of user input for validity. */
-
-		if (expr == null || expr.length() == 0){
-			//checks an empty input
-			throw new IllegalLineException ("You must extend the proof with " +
-											"an expression, or mp, ic, co, show, assume, or a theorem name " +
-											"followed by an expression.");
-
-		} if (expr.length() == 1) {
-			//if an expression has a length of 1, the only valid expression is a single variable
-			if (Character.isLetter(expr.charAt(0))){
-				return true;
-			} else {
-				throw new IllegalLineException("Single-character input must consist " +
-												"of only one variable.");
-			}
-
-		} if (expr.length() == 2) {
-			//if an expression has a length of 2, the only valid expression is ~variable
-			if (expr.charAt(0) == '~' && Character.isLetter(expr.charAt(1))){
-				return true;
-			} else {
-				throw new IllegalLineException("Two-character input must consist " +
-												"of only ~ and a variable.");
-			}
-
-		} else {
-			return expressionValidityHelper(expr);
+		//Strings with a length of one can only be variables
+		String c = new String("8");
+		try {
+			check.expressionValidity(c);
+			fail();
+		}
+		catch (IllegalLineException e) {
+			assert true;
+		}
+		
+		//checks String with a length of 2
+		String d = new String ("~p");
+		try {
+			check.expressionValidity(d);
+		//Strings with a length of 2 can only be in the format: '~variable'
+		}
+		catch (IllegalLineException e) {
+			fail();
+		}
+		
+		String z = new String ("89");
+		try {
+			check.expressionValidity(z);
+			fail();
+		}
+		catch (IllegalLineException e) {
+			assert true;
+		}
+		
+		String f = new String ("~8");
+		try {
+			check.expressionValidity(f);
+			fail();
+		}
+		catch (IllegalLineException e) {
+			assert true;
+		}
+		
+		String g = new String("~~");
+		try {
+			check.expressionValidity(g);
+			fail();
+		}
+		catch (IllegalLineException e) {
+			assert true;
+		}
+		
+		String h = new String("8t");
+		try {
+			check.expressionValidity(h);
+			fail();
+		}
+		catch (IllegalLineException e) {
+			assert true;
+		}
+		
+		String i = new String ("t~");
+		try {
+			check.expressionValidity(i);
+			fail();
+		}
+		catch (IllegalLineException e) {
+			assert true;
+		}
+		String j = new String ("(p=>q)");
+		try {
+			check.expressionValidity(j);
+		}
+		catch (IllegalLineException e) {
+			fail();
+		}
+		
+		String k = new String ("(p=>q=>p)");
+		//cant have more than one implies per parens
+		try {
+			check.expressionValidity(k);
+			fail();
+		}
+		catch (IllegalLineException e) {
+			assert true;
+		}
+		
+		String l = new String ("(p=>)q");
+		//We match up parens, but never check that there IS a closing paren
+		//in the right spot
+		//Only things that may follow a paren: => or )
+		try {
+			check.expressionValidity(l);
+			fail();
+		}
+		catch (IllegalLineException e) {
+			assert true;
+		}
+		
+		String m = new String("(p=>(q))");
+		try {
+			check.expressionValidity(m);
+			fail();
+		}
+		catch (IllegalLineException e) {
+			assert true;
+		}
+	
+ 		String n = new String("(p(=>q))");
+ 		try {
+ 			check.expressionValidity(n);
+ 			fail();
+ 		}
+		catch (IllegalLineException e) {
+			assert true;
+		}
+ 		
+		String o = new String("(p= >q)");
+		try {
+			check.expressionValidity(o);
+			fail();
+		}
+		catch (IllegalLineException e) {
+			assert true;
 		}
 	}
-
-	public boolean expressionValidityHelper(String expr) throws IllegalLineException {
-		//DO WE NEED THIS NULL CHECK?!!!!!!!!!!!
-
-			//checks if expression has an opening parenthesis at the second to last index or the very end 
-			if (expr.charAt(expr.length() - 1) == '(' || expr.charAt(expr.length() - 2) == '('){
-				throw new IllegalLineException("Your expression has a ( in an invalid place - " +
-												"too late in the expression.");
-			}
-
-			//checks if expression has a closing parenthesis in the first or second index
-			if (expr.charAt(0) == ')' || expr.charAt(1) == ')'){
-				return false;
-			}
-
-			for (int k = 0; k < expr.length(); k++){
-				//checks that we don't have a the following: ()
-				if (expr.charAt(k) == '(' && (k + 1 < expr.length() && expr.charAt(k+1) == ')')){
-					throw new IllegalLineException("You cannot enter an opening and closing " +
-													"parentheses consecutively.");
-				}
-
-				//checks that all parentheses are matched
-				if (expr.charAt(k) == '(') {
-					charStack.push(expr.charAt(k));
-				} else if (expr.charAt(k) == ')'){
-					charStack.pop();
-				}	
-				if (k == expr.length() - 1 && !expr.isEmpty()) {
-					throw new IllegalLineException("Your parentheses are unmatched.");
-				}
-
-
-				//checks surrounding characters when encountering variables
-				if (Character.isLetter(expr.charAt(k))){
-					if (k-1 >= 0){
-						//checks possible characters that precede the current character
-						if (expr.charAt(k-1) != '|' 
-								|| expr.charAt(k-1) != '&'
-								|| expr.charAt(k-1) != '~'
-								|| expr.charAt(k-1) != '('
-								//double check if we want to keep this paren check because we are substringing
-								|| (expr.charAt(k-1) != '>' && k-2 >= 0 && expr.charAt(k-2) != '=')){
-							throw new IllegalLineException ("There is an invalid character preceding the variable."); 
-						}
-					if (k+1 < expr.length()){
-						if (expr.charAt(k+1) != '|' 
-								|| expr.charAt(k+1) != '&'
-								|| expr.charAt(k+1) != ')'
-								|| (expr.charAt(k+1) != '=' && k+2 < expr.length() && expr.charAt(k+2) != '>')){
-							throw new IllegalLineException ("There is an invalid character following the variable.");
-						}
-					}
-
-					
-				//checks surrounding characters when encountering '&' or '|'
-				if (expr.charAt(k) == '&' || expr.charAt(k) == '|'){
-					if (k-1 >= 0){
-						if (!Character.isLetter(expr.charAt(k-1)) 
-								|| expr.charAt(k-1) != ')'){
-							throw new IllegalLineException ("There is an invalid character preceding & or |.");
-						}
-					}
-					if (k+1 < expr.length()){
-						if (Character.isLetter(expr.charAt(k+1))
-								|| expr.charAt(k+1) != '~'
-								|| expr.charAt(k+1) != '('){
-							throw new IllegalLineException ("There is an invalid character following & or |");
-						}
-					}
-
-					
-				//checks surrounding characters when encountering '~'
-				if (expr.charAt(k) == '~'){
-					if (k-1 >= 0){
-						//checks possible characters that precede the current character
-						if (expr.charAt(k-1) != '|' 
-								|| expr.charAt(k-1) != '&'
-								|| expr.charAt(k-1) != '~'
-								|| expr.charAt(k-1) != '('
-								//double check if we want to keep this paren check because we are substringing
-								|| (expr.charAt(k-1) != '>' && k-2 >= 0 && expr.charAt(k-2) != '=')){
-							throw new IllegalLineException ("There is an invalid character preceding ~."); 
-						}
-					if (k+1 < expr.length()){
-						if (!Character.isLetter(expr.charAt(k+1)) 
-								|| expr.charAt(k+1) != '~'){
-							throw new IllegalLineException ("There is an invalid character following ~.");
-						}
-					}
-
-					
-				//checks condition when encountering the '=' of '=>'
-				if (expr.charAt(k) == '='){
-					if (k-1 >= 0){
-						if (!Character.isLetter(expr.charAt(k-1))
-								|| expr.charAt(k-1) != ')'){
-							throw new IllegalLineException ("There is an invalid character preceding =.");
-						}
-					}
-					if (expr.charAt(k+1) != '>'){
-						throw new IllegalLineException ("> must always follow =.");
-					}
-				}
-				
-				//checks condition when encountering the '>' of '=>'
-				if (expr.charAt(k) == '>'){
-					if (expr.charAt(k-1) != '='){
-						throw new IllegalLineException ("= must always precede >.");
-					}
-					if (k+1 < expr.length()){
-						if (!Character.isLetter(expr.charAt(k+1)) 
-								|| expr.charAt(k+1) != '~'
-								|| expr.charAt(k+1) != '('){
-							throw new IllegalLineException ("There is an invalid character following >.");
-						}
-					}
-				}
-				
-				
-				//cases for ')' - must be followed by =, ), or be at the end of the expression
-				//')' or a letter may precede ')'
-				if (expr.charAt(k) == ')') {
-					if (expr.charAt(k+1) != '=' || expr.charAt(k+1) != ')'
-					 || k != expr.length()-1) {
-						throw new IllegalLineException("The only elements that may follow "+
-														"a ) are => or another ); otherwise," +
-														"you must be at the end of the expression.");
-					}
-					if (Character.isLetter(expr.charAt(k-1)) == false || expr.charAt(k-1) != ')') {
-						throw new IllegalLineException("The only elements that may precede "
-													+ "a ) are a variable, or another ); otherwise, " +
-													"you must be at the end of the expression.");
-					}
-				}
-				if (expr.charAt(k) == '(') {
-					//check recursion depth???
-					return expressionValidityHelper(expr.substring(k, expr.length()));
-				}
-			}
-			return true;
-		}
-	}
+}
