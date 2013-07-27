0ee0dbfebd3327ee0357b7f6c462ddf5025f228f
kchao1994 <kchao1994@gmail.com>
2013-07-26 22:41:04 -0700

    Update LineNumber.java

--- LineNumber.java
+++ LineNumber.java
@@ -1,13 +1,11 @@
-
 import java.util.ArrayList;
 
 /**
-* LineNumber
-* Produces the line numbers of the proof
-* Each line number forms its own ArrayList
-**/
+ *  LineNumber
+ *  Produces the line numbers of the proof
+ *  Each line number forms its own ArrayList
+ **/
 public class LineNumber {
-	
 	//Instance variables
 	private ArrayList<Integer> numbers;
 	private boolean beginProof;
@@ -15,12 +13,12 @@ public class LineNumber {
 	private boolean justStarting;
 	
 	/**
-	* LineNumer Initiates a new LineNumber object.
-	* Creates an ArrayList to store all of the line numbers.
-	*
-	* @param beginProof
-	* @param finishProof
-	**/
+	 *  LineNumer Initiates a new LineNumber object.
+	 *  Creates an ArrayList to store all of the line numbers.
+	 *  
+	 *  @param beginProof
+	 *  @param finishProof
+	 **/
 	public LineNumber(boolean beginProof, boolean finishProof) {
 		this.beginProof = beginProof;
 		this.finishProof = finishProof;
@@ -29,28 +27,28 @@ public class LineNumber {
 	}
 	
 	/**
-	* setBeginProof() Sets the beginProof field;
-	*
-	* @param newBoolean What to set beginProof to
-	**/
+	 *  setBeginProof() Sets the beginProof field;
+	 *  
+	 *  @param newBoolean What to set beginProof to
+	 **/
 	public void setBeginProof(boolean newBoolean) {
 		this.beginProof = newBoolean;
 	}
 	
 	/**
-	* setFinishProof() Sets the finishProof field.
-	*
-	* @param newBoolean What to set finishProof to.
-	**/
+	 *  setFinishProof() Sets the finishProof field.
+	 *  
+	 *  @param newBoolean What to set finishProof to.
+	 **/
 	public void setFinishProof(boolean newBoolean) {
 		this.finishProof = newBoolean;
 	}
 	
 	/**
-	* next() Creates the next line number.
-	*
-	* @return LineNumber the LineNumber object
-	**/
+	 *  next() Creates the next line number.
+	 *  
+	 *  @return LineNumber the LineNumber object
+	 **/
 	public String next() {
 		if(justStarting){
 			numbers.add(1);
@@ -74,10 +72,10 @@ public class LineNumber {
 	}
 	
 	/**
-	* getCurrent() Returns the current lineNumber without modifying the LineNumber object
-	*
-	* @return String Next line number
-	**/
+	 *  getCurrent() Returns the current lineNumber without modifying the LineNumber object
+	 *  
+	 *  @return String Next line number
+	 **/
 	public String getCurrent(){
 		return this.toString();
 	}
@@ -99,30 +97,33 @@ public class LineNumber {
 		return result;
 	}
 	/**
-	* size() returns the size of the line object
-	*
-	* @return int Size of LineNumber
-	**/
+	 *  size() returns the size of the line object
+	 *  
+	 *  @return int Size of LineNumber
+	 **/
 	public int size(){
 		return numbers.size();
 	}
 	
+	public void decrement() {
+		Integer lastNum = numbers.get(numbers.size() - 1);
+		numbers.set(numbers.size()-1, new Integer(lastNum - 1));
+	}
+	
 	/**
-	* prev() Decrements the line number;
-	**/
+	 *  prev() Decrements the line number;
+	 **/
 	public String prev(){
-		if(numbers.size() == 1){
-		}
 		Integer newNum = numbers.get(numbers.size()-1) - 1;
 		numbers.set(numbers.size()-1, newNum);
 		return this.toString();
 	}
 	
 	/**
-	* toString() Returns the string representation of a LineNumber
-	*
-	* @return result String representation of the LineNumber
-	**/
+	 *  toString() Returns the string representation of a LineNumber
+	 *  
+	 *  @return result String representation of the LineNumber
+	 **/
 	public String toString() {
 		String result = "" + numbers.get(0);
 		for(int i=1; i<numbers.size(); i++){
@@ -132,51 +133,52 @@ public class LineNumber {
 	}
 	
 	public static boolean isValidReference(String refLine, String currLine) {
-		  
-			try {
-			int firstrefNum = Integer.parseInt(refLine.substring(0, 1));
-			int firstcurrNum = Integer.parseInt(currLine.substring(0, 1));
-			if(firstrefNum != firstcurrNum) {
-				if(firstrefNum > firstcurrNum || refLine.length() != 1) {
+	  
+		try {
+		int firstrefNum = Integer.parseInt(refLine.substring(0, 1));
+		int firstcurrNum = Integer.parseInt(currLine.substring(0, 1));
+		if(firstrefNum != firstcurrNum) {
+			if(firstrefNum > firstcurrNum || refLine.length() != 1) {
+				return false;
+			}
+		}
+		
+		if(refLine.length() == currLine.length()) {
+			for(int i = 0; i < currLine.length() - 1; i+= 2) {
+				int refCurrNum = Integer.parseInt(refLine.substring(i, i + 1));
+				int currCurrNum = Integer.parseInt(currLine.substring(i, i + 1));
+				if(refCurrNum != currCurrNum) {
 					return false;
 				}
 			}
-			
-			if(refLine.length() == currLine.length()) {
-				for(int i = 0; i < currLine.length() - 1; i+= 2) {
-					int refCurrNum = Integer.parseInt(refLine.substring(i, i + 1));
-					int currCurrNum = Integer.parseInt(currLine.substring(i, i + 1));
-					if(refCurrNum != currCurrNum) {
-						return false;
-					}
+		}
+		
+		if(refLine.length() > currLine.length()) {
+			return false;
+		}
+		
+		if(refLine.length() < currLine.length()) {
+			for(int i = 0; i < refLine.length() - 1; i+= 2) {
+				int refCurrNum = Integer.parseInt(refLine.substring(i, i + 1));
+				int currCurrNum = Integer.parseInt(currLine.substring(i, i + 1));
+				if(refCurrNum > currCurrNum) {
+					return false;
 				}
 			}
-			
-			if(refLine.length() > currLine.length()) {
-				return false;
-			}
-			
-			if(refLine.length() < currLine.length()) {
-				for(int i = 0; i < refLine.length() - 1; i+= 2) {
-					int refCurrNum = Integer.parseInt(refLine.substring(i, i + 1));
-					int currCurrNum = Integer.parseInt(currLine.substring(i, i + 1));
-					if(refCurrNum > currCurrNum) {
-						return false;
-					}
-				}
-			}
-	
-			int refLastNum = Integer.parseInt(refLine.substring(refLine.length() - 1));
-			int currLastNum = Integer.parseInt(currLine.substring(currLine.length() - 1));;
-			if(currLastNum < refLastNum) {
-				return false;
-			}
-			
-			return true;
-	  }
-		catch (NumberFormatException e) {
-			System.out.println("Error in line number");
+		}
+
+		int refLastNum = Integer.parseInt(refLine.substring(refLine.length() - 1));
+		int currLastNum = Integer.parseInt(currLine.substring(currLine.length() - 1));;
+		if(currLastNum < refLastNum) {
+			return false;
+		}
+		
+		return true;
+  }
+	catch (NumberFormatException e) {
+		System.out.println("Error in line number");
 	}
 	return false;
 	}
 }
+
